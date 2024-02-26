7c9135342d049d4026c165e29f31695a0a124dce
zyxkr <sirilucky.reddy111@gmail.com>
2024-02-26 14:23:15 +0000

    Code giving error

---
+++ src/components/context/ThemeAndVideoContext.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const ThemeAndVideoContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  activeTab: 'Home',
+  toggleTheme: () => {},
+  changeTab: () => {},
+  addVideo: () => {},
+})
+export default ThemeAndVideoContext
